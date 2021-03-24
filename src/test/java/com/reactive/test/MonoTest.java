175a5bd545bbf2ea78f683ff3485dc96c4760062
Luiz Geraldo Pinheiro <luizpinheiro.rs@gmail.com>
2021-03-24 09:15:42 -0300

    first commit

---
+++ src/test/java/com/reactive/test/MonoTest.java
@@ -0,0 +1,130 @@
+package com.reactive.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@Slf4j
+/**
+ * Reactive streams
+ * 1. Async
+ * 2. Non-Blocking
+ * 3. Backpressure
+ * Publisher -> chef <- (Subscriber)
+ * Subscription is created
+ * Publisher (onSubscribe with the subscription) -> Subscriber
+ * Subscription <- (request N) Subscriber
+ * Publisher -> (onNext) Subscriber
+ * until:
+ * 1. Publisher sends all the objects requested
+ * 2. Publisher sends all the objects it has. (onComplete) subscriber and subscription will be canceled
+ * 3. There is an error. (onError) subscriber and subscription will be canceled
+ */
+public class MonoTest {
+
+    @Test
+    public void monoSubscriber() {
+        String name = "Luiz Pinheiro";
+        Mono<String> mono = Mono.just(name)
+                .log();
+
+        mono.subscribe();
+
+        log.info("--------------------");
+        StepVerifier.create(mono)
+                .expectNext(name)
+                .verifyComplete();
+    }
+
+    @Test
+    public void monoSubscriberConsumer() {
+        String name = "Luiz Pinheiro";
+        Mono<String> mono = Mono.just(name)
+                .log();
+
+        mono.subscribe(s -> log.info("Value {}", s));
+
+        log.info("--------------------");
+        StepVerifier.create(mono)
+                .expectNext(name)
+                .verifyComplete();
+    }
+
+    @Test
+    public void monoSubscriberConsumerError() {
+        String name = "Luiz Pinheiro";
+        Mono<String> mono = Mono.just(name)
+                .map(s -> {
+                    throw new RuntimeException("Testing mono with error");
+                });
+
+        mono.subscribe(s -> log.info("Name {}", s), s -> log.error("Something bad happened"));
+        mono.subscribe(s -> log.info("Name {}", s), Throwable::printStackTrace);
+
+        log.info("--------------------");
+        StepVerifier.create(mono)
+                .expectError(RuntimeException.class)
+                .verify();
+    }
+
+    @Test
+    public void monoSubscriberConsumerComplete() {
+        String name = "Luiz Pinheiro";
+        Mono<String> mono = Mono.just(name)
+                .log()
+                .map(String::toUpperCase);
+
+        mono.subscribe(s -> log.info("Value {}", s),
+                Throwable::printStackTrace,
+                () -> log.info("FINISHED!"));
+
+        log.info("--------------------");
+        StepVerifier.create(mono)
+                .expectNext(name.toUpperCase())
+                .verifyComplete();
+    }
+
+    @Test
+    public void monoSubscriberConsumerSubscription() {
+        String name = "Luiz Pinheiro";
+        Mono<String> mono = Mono.just(name)
+                .log()
+                .map(String::toUpperCase);
+
+        mono.subscribe(s -> log.info("Value {}", s),
+                Throwable::printStackTrace,
+                () -> log.info("FINISHED!"),
+//                Subscription::cancel);
+                subscription -> subscription.request(5));
+
+        log.info("--------------------");
+        StepVerifier.create(mono)
+                .expectNext(name.toUpperCase())
+                .verifyComplete();
+    }
+
+    @Test
+    public void monoDoOnMethods() {
+        String name = "Luiz Pinheiro";
+        Mono<Object> mono = Mono.just(name)
+                .log()
+                .map(String::toUpperCase)
+                .doOnSubscribe(subscription -> log.info("Subscribed"))
+                .doOnRequest(longNumber -> log.info("Request received, starting doing something"))
+                .doOnNext(s -> log.info("Value is here. Executing doOnNext {}", s))
+                .flatMap(s -> Mono.empty())
+                .doOnNext(s -> log.info("Value is here. Executing doOnNext {}", s)) // will not be executed
+                .doOnSuccess(s -> log.info("doOnSuccess executed {}", s));
+
+        mono.subscribe(s -> log.info("Value {}", s),
+                Throwable::printStackTrace,
+                () -> log.info("FINISHED!"));
+
+        log.info("--------------------");
+//        StepVerifier.create(mono)
+//                .expectNext(name.toUpperCase())
+//                .verifyComplete();
+    }
+}
